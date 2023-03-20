1ca0b81590f367ee2fc5a1b2cc868af3a8bb2283
kitrath <crathmel@gmail.com>
2023-03-19 21:07:32 -0500

    checkpoint commit, server start, routes wired

--- controllers/pageRoutes.js
+++ controllers/pageRoutes.js
@@ -1,12 +1,21 @@
 const router = require('express').Router();
 const { BlogPost, Comment, User } = require('../models');
-const withAuth = reqire('../utils/auth');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
-        const BlogPostData = await PromiseRejectionEvent.findAll({
+        const blogPostData = await PromiseRejectionEvent.findAll({
             attibutes: ['id', 'title', 'snippet', 'createdAt'],
             include: [{ model: User, attributes: ['name'] }],
         });
+
+        const blogPosts = blogPostData.map((blogPost) => {
+            return blogPost.get({ plain: true }); 
+        });
+    
+    } catch (err) {
+        console.error(err);
     }
-})
+});
+
+module.exports = router;
---
+++ utils/helpers.js
@@ -0,0 +1,5 @@
+const format_date = (date) => {
+    return date.toLocaleString();
+}
+
+module.exports = { format_date };
