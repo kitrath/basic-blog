f22200756fb6d76a0887bf7819ff1dfba8714a41
kitrath <crathmel@gmail.com>
2023-03-19 20:51:56 -0500

    add router imports, structure

--- controllers/pageRoutes.js
+++ controllers/pageRoutes.js
@@ -1,3 +1,12 @@
 const router = require('express').Router();
 const { BlogPost, Comment, User } = require('../models');
 const withAuth = reqire('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const BlogPostData = await PromiseRejectionEvent.findAll({
+            attibutes: ['id', 'title', 'snippet', 'createdAt'],
+            include: [{ model: User, attributes: ['name'] }],
+        });
+    }
+})
