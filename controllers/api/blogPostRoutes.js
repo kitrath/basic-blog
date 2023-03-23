5cd9b6775271b0d996c07e40d3abf094c0cb50ff
kitrath <crathmel@gmail.com>
2023-03-23 12:19:20 -0500

    add get by id route to blog post api

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -4,6 +4,31 @@ const withAuth = require('../../utils/auth');
 
 module.exports = router;
 
+// To retrieve posts to edit in /dashboard
+router.get('/:id', withAuth, async (req, res) => {
+    try {
+        const blogPostId = req.params.id;
+        const blogPost = await BlogPost.findByPk(blogPostId);
+
+        if (!blogPost) {
+            res.status(404).json({ success: false, message: 'Not found' });
+            return;
+        }
+
+        res.status(200).json({
+            success: true,
+            blog_post: {
+                id: blogPost.id,
+                title: blogPost.title,
+                content: blogPost.content
+            },
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(400).json(err);
+    }
+});
+
 // req.body = {title, content}
 router.post('/', withAuth, async (req, res) => {
     try {
@@ -30,9 +55,7 @@ router.put('/:id', withAuth, async (req, res) => {
     try {
         const blogPostId = req.params.id;
         const updatedBlogPost = BlogPost.update(req.body, {
-            where: {
-                id: blogPostId,
-            }
+            where: { id: blogPostId }, 
         });
 
         if (!updatedBlogPost) {
@@ -60,8 +83,14 @@ router.delete('/:id', withAuth, async (req, res) => {
         if (!blogPost) {
             res.status(404).json({ success: false, message: 'Not found' });
             return;
-        } else {
+        } 
 
-        }
+        res.status(200).json({
+            success: true,
+            message: `Successfully deleted blog post with id ${blogPostId}`
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(400).json(err);
     }
 });
